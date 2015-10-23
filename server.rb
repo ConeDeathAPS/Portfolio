8942db9f8958c761c7aa095a6a18c6ff438502f5
ConeDeathAPS <sorensenapaul@gmail.com>
2015-10-23 15:27:43 -0700

    initial commit

---
+++ server.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+
+class Project < Sinatra::Base
+end
+
+get '/' do
+	erb :index
+end
+
+get '/projects' do
+	erb :projects
+end
+
+get '/music' do
+	erb :music
+end
+
+get '/project' do
+	erb :in_progress
+end
+
+get '/bio' do
+	erb :in_progress
+end
